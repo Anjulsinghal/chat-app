182a35683da4e13d4c5152471898a4aa7c1b751e
Anjulsinghal <anjulsinghal123@gmail.com>
2024-12-15 23:33:25 +0530

    change to production

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,13 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
